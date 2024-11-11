8f22c6989dbf09d11a6bae59dabd6b4e9a6a42fc
Joshua <horheyinc@gmail.com>
2024-11-11 21:56:57 +0100

    store

--- src/components/stores/DecrementStore.svelte
+++ src/components/stores/DecrementStore.svelte
@@ -1,6 +1,7 @@
 <script>
   import { count } from "./store";
   function decrement() {
+    // call the count .update to adjust the value. You get a callback with the previous value as the argument
     count.update((prev) => {
       return (prev -= 1);
     });
--- src/components/stores/DisplayStore.svelte
+++ src/components/stores/DisplayStore.svelte
@@ -3,9 +3,11 @@
   import { count } from "../stores/store";
 
   let counter;
+  // store the subscribe in a variable to be destroyed when the component is unmounted. ($count will also do the same )
   const idToUnsubscribe = count.subscribe((prevValue) => {
     counter = prevValue;
   });
+  // this is for when the component is unmounted so no memory leaks
   onDestroy(idToUnsubscribe);
 </script>
 
